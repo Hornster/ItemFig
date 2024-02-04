42337b85a09226aab84df7c5c7ab1ead3e0b2b87
Hornster <karol199714@gmail.com>
2024-02-04 15:33:57 +0100

    1.19.2 Fabric configuration done

---
+++ src/main/java/io/github/hornster/itemfig/ItemFigDataGenerator.java
@@ -0,0 +1,11 @@
+package io.github.hornster.itemfig;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ItemFigDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
